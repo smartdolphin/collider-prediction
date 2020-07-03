e2d3732fca9170af0c3964677c2f4773a5bd8bff
Taeho Lee <smartdolphin07@gmail.com>
2020-07-03 10:30:15 +0000

    Update run script
    Signed-off-by: Taeho Lee <smartdolphin07@gmail.com>

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-#python -u train.py -g 1 -b 256 -e 1000 -o model1
-#python -u train.py -g 2 -b 256 -e 1000 -o model2
-#python -u train.py -g 3 -b 256 -e 1000 -o model3
-python -u train.py -g 0 -b 400 -e 1500 -o model4
+#python -u train.py -g 1 -b 256 -e 1000 -o model1 --start 1
+#python -u train.py -g 2 -b 256 -e 1000 -o model2 --start 1
+#python -u train.py -g 3 -b 256 -e 1000 -o model3 --start 1
+#python -u train.py -g 0 -b 400 -e 1500 -o model4 --start 1
+
+python -u train.py -g 2 -b 256 -e 1500 -o model_xy -t 0 --start 1
